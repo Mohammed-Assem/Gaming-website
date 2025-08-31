b355501d6ed9e503b08991bed9d9f1b63f17062c
Mohammed-Assem <mohamedassem495@gmail.com>
2025-08-31 14:38:48 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -31,3 +31,59 @@ const buttonhover = function (event) {
 }
 
 addEventOnElements(button, "mousemove", buttonhover);
+
+
+
+const header = document.querySelector("[header]");
+const backtop = document.querySelector("[back]");
+
+const active = function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+    backtop.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backtop.classList.remove("active");
+  }
+}
+
+window.addEventListener("scroll", active);
+
+
+
+const reveal = document.querySelectorAll("[data-reveal]");
+
+const revealElementOnScroll = function () {
+  for (let i = 0, len = reveal.length; i < len; i++) {
+    const isElementInsideWindow = reveal[i].getBoundingClientRect().top < window.innerHeight / 1.1;
+
+    if (isElementInsideWindow) {
+      reveal[i].classList.add("revealed");
+    }
+  }
+}
+
+window.addEventListener("scroll", revealElementOnScroll);
+
+window.addEventListener("load", revealElementOnScroll);
+
+
+
+
+const cursor = document.querySelector("[cursor]");
+const hover = [...document.querySelectorAll("a"), ...document.querySelectorAll("button")];
+
+const move = function (event) {
+  cursor.style.top = `${event.clientY}px`;
+  cursor.style.left = `${event.clientX}px`;
+}
+
+window.addEventListener("mousemove", move);
+
+addEventOnElements(hover, "mouseover", function () {
+  cursor.classList.add("hovered");
+});
+
+addEventOnElements(hover, "mouseout", function () {
+  cursor.classList.remove("hovered");
+});
